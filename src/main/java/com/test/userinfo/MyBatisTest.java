99a53451cd3ac70bf65ff77ee3e3925031e993a0
songxin07 <18811437348@163.com>
2017-06-17 16:31:31 +0800

    mybatis add user

---
+++ src/main/java/com/test/userinfo/MyBatisTest.java
@@ -0,0 +1,29 @@
+package com.test.userinfo;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.test.userinfo.entity.User;
+/**
+ * Created by songxin07 on 17/6/17.
+ */
+public class MyBatisTest {
+    public static void main(String[] args) throws Exception {
+        // 读取mybatis-config.xml文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 初始化mybatis，创建SqlSessionFactory类的实例
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+                .build(inputStream);
+        // 创建Session实例
+        SqlSession session = sqlSessionFactory.openSession();
+        // 创建User对象
+        User user = new User("admin", "男", 26);
+        // 插入数据
+        session.insert("test.save", user);
+        // 提交事务
+        session.commit();
+        // 关闭Session
+        session.close();
+    }
+}
