b0fcf735264edf4fa4ad93e47b693290bf68c3ae
sxin1210 <448590223@qq.com>
2017-07-05 17:22:39 +0800

    Update UserserviceImpl.java

--- src/main/java/com/test/userinfo/service/impl/UserserviceImpl.java
+++ src/main/java/com/test/userinfo/service/impl/UserserviceImpl.java
@@ -1,7 +1,32 @@
 package com.test.userinfo.service.impl;
 
+import com.test.userinfo.entity.User;
+import com.test.userinfo.mapper.UserMapper;
+import com.test.userinfo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
- * Created by songxin07 on 17/6/15.
+ * Created by songxin07 on 17/6/20.
  */
-public class UserserviceImpl {
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserMapper userMapper;
+    //    @Override
+    public User login(String loginname, String password) {
+        return userMapper.findWithLoginnameAndPassword(loginname,password);
+    }
+    public int register(String loginname, String password, String username,String phone,String address){
+        return userMapper.addUser(loginname,password,username,phone,address);
+    }
+
+    public int updateUserInfo(String username,int id){
+        return userMapper.updateUserNameByID(username,id);
+    }
+
+    public int deleteUserByID(int id){
+        return userMapper.deleteUserByID(id);
+    }
 }
